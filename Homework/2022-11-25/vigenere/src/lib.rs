05ce824061eb6ff154acb381dc66ba6ed48cb544
Jeffrey Goldberg <jeff@agilebits.com>
2022-11-20 13:40:24 -0600

    Getting way longer than 100 lines

--- Homework/2022-11-25/vigenere/src/lib.rs
+++ Homework/2022-11-25/vigenere/src/lib.rs
@@ -10,16 +10,27 @@
 
 use std::collections::HashMap;
 
+const DEFAULT_ABC: &str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
 /// Errors happen. We might report them.
 pub enum Error {
     /// Something bad happened
     ErrorOps,
+
+    /// Duplicate characters in alphabet
+    ErrorDupChars,
+
+    /// Key is too short
+    ErrorShortKey,
+
+    /// Key has chars not in alphabet
+    ErrorBadKeyChar,
 }
 
 type AtoIMap = HashMap<char, usize>;
 type ItoAMap = HashMap<usize, char>;
 
-/// Alphabet is the ordered set of characters we will cope with
+/// Alphabet is the ordered set of characters we will cope with.
 pub struct Alphabet {
     value: String,
     length: usize,
@@ -27,74 +38,93 @@ pub struct Alphabet {
     itoa_map: HashMap<usize, char>,
 }
 
+impl Default for Alphabet {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
 impl Alphabet {
-    fn build_maps(alphabet: &str) -> (AtoIMap, ItoAMap) {
+    // builds the position/character associative arrays
+    // also validates that the input alphabet doesn't have duplicates
+    fn build_maps(alphabet: &str) -> Result<(AtoIMap, ItoAMap), Error> {
         let mut atoi: AtoIMap = HashMap::new();
         let mut itoa: ItoAMap = HashMap::new();
 
         for (i, c) in alphabet.chars().enumerate() {
+            if atoi.contains_key(&c) {
+                return Err(Error::ErrorDupChars);
+            }
             atoi.insert(c, i);
             itoa.insert(i, c);
         }
-        (atoi, itoa)
+        Ok((atoi, itoa))
     }
-}
 
-impl Default for Alphabet {
-    fn default() -> Self {
-        let value: String = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".to_string();
-        let length: usize = 26;
-        let (atoi_map, itoa_map) = Self::build_maps(&value);
-
-        Self {
-            value,
-            length,
-            atoi_map,
-            itoa_map,
+    /// checks that every char in key is in alphabet
+    pub fn is_key_valid(&self, key: &str) -> bool {
+        for c in key.chars() {
+            if !self.atoi_map.contains_key(&c) {
+                return false;
+            }
         }
+        true
     }
 }
 
 impl Alphabet {
-    fn new() -> Self {
-        Default::default()
+    pub fn new() -> Self {
+        match Self::try_from(DEFAULT_ABC.to_string()) {
+            Err(_) => panic!("default should not error"),
+            Ok(abc) => abc,
+        }
     }
 
-    fn add(&self, a: char, b: char) -> Option<char> {
-        let a_pos = self.atoi_map.get(&a)?;
-        let b_pos = self.atoi_map.get(&b)?;
+    // We do input validation when constructing objects,
+    // so safe to panic here if things are malformed.
+    fn add(&self, a: char, b: char) -> char {
+        let a_pos = self.atoi_map.get(&a).unwrap();
+        let b_pos = self.atoi_map.get(&b).unwrap();
         let new_pos = (a_pos + b_pos) % self.length;
-        let c = self.itoa_map.get(&new_pos)?;
-        Some(*c)
+        let c = self.itoa_map.get(&new_pos).unwrap();
+        *c
+    }
+    fn sub(&self, a: char, b: char) -> char {
+        self.add(a, self.inv(b))
     }
     #[inline]
-    fn neg(&self, pos: usize) -> usize {
-        self.length - pos
-    }
-    fn sub(&self, a: char, b: char) -> Option<char> {
-        let a_pos = self.atoi_map.get(&a)?;
-        let b_pos = self.atoi_map.get(&b)?;
-        let new_pos = (a_pos + self.neg(*b_pos)) % self.length;
-        let c = self.itoa_map.get(&new_pos)?;
-        Some(*c)
+    // return the additive inverse of a character given the alphabet
+    fn inv(&self, c: char) -> char {
+        let pos = self.atoi_map.get(&c).unwrap();
+        let neg_pos = self.length - pos;
+        let inv_c = self.itoa_map.get(&neg_pos).unwrap();
+        *inv_c
     }
 }
 
 impl TryFrom<String> for Alphabet {
     type Error = Error;
+    /// Will Error if s contains duplicate characters or if s is shorter than 2
     fn try_from(s: String) -> Result<Self, Error> {
-        // validiate_alphabet(s)?
+        if s.len() < 2 {
+            return Err(Error::ErrorOps);
+        }
         let value = s.clone();
         let length = s.len();
-        let (atoi_map, itoa_map) = Self::build_maps(&s);
-        Ok(Self {value, length, atoi_map, itoa_map})
+        let (atoi_map, itoa_map) = Self::build_maps(&s)?;
+        Ok(Self {
+            value,
+            length,
+            atoi_map,
+            itoa_map,
+        })
     }
 }
 
 /// Vigenere is where we will put our key and alphabet
 pub struct Vigenere {
     /// The alphabet is an ordered sequence of unique characters.
-    pub alphabet: Alphabet,
+    alphabet: Alphabet,
 
     // The key must be a string of characters all within the alphabet
     key: String,
@@ -102,7 +132,7 @@ pub struct Vigenere {
 
 impl Vigenere {
     pub fn new(key: &str) -> Result<Self, Error> {
-        let alphabet: Alphabet = Default::default();
+        let alphabet: Alphabet = Alphabet::new();
         Ok(Self {
             alphabet,
             key: key.into(),
@@ -110,9 +140,16 @@ impl Vigenere {
     }
 
     pub fn new_with_alphabet(key: &str, alphabet: String) -> Result<Self, Error> {
-        let key = key.to_string();
-        // validate_key(key)?
         let alphabet: Alphabet = Alphabet::try_from(alphabet)?;
-        Ok(Self { key, alphabet })
+        if key.is_empty() {
+            return Err(Error::ErrorShortKey);
+        }
+        if !alphabet.is_key_valid(key) {
+            return Err(Error::ErrorBadKeyChar);
+        }
+        Ok(Self {
+            key: key.to_string(),
+            alphabet,
+        })
     }
 }
