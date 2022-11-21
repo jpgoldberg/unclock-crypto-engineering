261fff247972104e27351d16e6f4c96c5901c5c5
Jeffrey Goldberg <jeff@agilebits.com>
2022-11-20 22:04:44 -0600

    Strings make clippy happy

--- Homework/2022-11-25/vigenere/src/lib.rs
+++ Homework/2022-11-25/vigenere/src/lib.rs
@@ -9,7 +9,7 @@
 #![allow(dead_code)]
 
 use std::collections::HashMap;
-use unicode_segmentation::{UnicodeSegmentation};
+use unicode_segmentation::UnicodeSegmentation;
 
 pub const DEFAULT_ABC: &str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
@@ -31,73 +31,73 @@ pub enum Error {
     ErrorShortABC,
 }
 
-pub struct Key<'a> {
-    value: Vec<&'a str>,
+pub struct Key {
+    value: Vec<String>,
 }
 
-impl From<&str> for Key<'_> {
+impl From<&str> for Key {
     fn from(s: &str) -> Self {
-        Self::from(s.to_string())   
+        Self::from(s.to_string())
     }
 }
-impl From<String> for Key<'_> {
+impl From<String> for Key {
     fn from(s: String) -> Self {
-        let v: Vec<&str> = s
+        let value: Vec<String> = s
             .to_uppercase()
             .graphemes(true)
+            .map(|s| s.to_string())
             .collect();
-        Self {value: v.clone()}
+        Self { value }
     }
 }
 
-impl Key<'_> {
+impl Key {
     fn len(&self) -> usize {
         self.value.len()
     }
 
-    fn char_from_position(&self, pos: usize) -> &str {
+    fn char_from_position(&self, pos: usize) -> String {
         let pos = pos % self.len();
-        self.value[pos]
+        self.value[pos].clone()
     }
 }
 
-type AtoIMap<'a> = HashMap<&'a str, usize>;
-type ItoAMap<'a> = HashMap<usize, &'a str>;
-
+type AtoIMap = HashMap<String, usize>;
+type ItoAMap = HashMap<usize, String>;
 
 /// Alphabet is the ordered set of characters we will cope with.
-pub struct Alphabet<'a> {
-    value: Vec<&'a str>,
+pub struct Alphabet {
+    value: Vec<String>,
     length: usize,
-    atoi_map: AtoIMap<'a>,
-    itoa_map: ItoAMap<'a>,
+    atoi_map: AtoIMap,
+    itoa_map: ItoAMap,
 }
 
-impl Default for Alphabet<'_> {
+impl Default for Alphabet {
     fn default() -> Self {
         Self::new()
     }
 }
 
-impl Alphabet<'_> {
+impl Alphabet {
     // builds the position/character associative arrays
     // also validates that the input alphabet doesn't have duplicates
-    fn build_maps<'a>(alphabet: Vec<&'a str>) -> Result<(AtoIMap<'a>, ItoAMap<'a>), Error> {
+    fn build_maps(alphabet: Vec<String>) -> Result<(AtoIMap, ItoAMap), Error> {
         let mut atoi: AtoIMap = HashMap::new();
         let mut itoa: ItoAMap = HashMap::new();
 
         for (i, c) in alphabet.into_iter().enumerate() {
-            if atoi.contains_key(c) {
+            if atoi.contains_key(&c) {
                 return Err(Error::ErrorDupChars);
             }
-            atoi.insert(c, i);
+            atoi.insert(c.clone(), i);
             itoa.insert(i, c);
         }
         Ok((atoi, itoa))
     }
 
-    pub fn contains(&self, c: &str) -> bool {
-        self.atoi_map.contains_key(c)
+    pub fn contains(&self, c: String) -> bool {
+        self.atoi_map.contains_key(&c)
     }
     /// checks that every char in key is in alphabet
     pub fn is_key_valid(&self, key: Key) -> bool {
@@ -110,7 +110,7 @@ impl Alphabet<'_> {
     }
 }
 
-impl Alphabet<'_> {
+impl Alphabet {
     /// Creates an Alphabet using the [DEFAULT_ABC]
     pub fn new() -> Self {
         match Self::try_from(DEFAULT_ABC.to_string()) {
@@ -121,40 +121,43 @@ impl Alphabet<'_> {
 
     // We do input validation when constructing objects,
     // so safe to panic here if things are malformed.
-    fn add(&self, a: &str, b: &str) -> &str {
+    fn add(&self, a: String, b: String) -> String {
         let a_pos = self.atoi_map.get(&a).unwrap();
         let b_pos = self.atoi_map.get(&b).unwrap();
         let new_pos = (a_pos + b_pos) % self.length;
         let c = self.itoa_map.get(&new_pos).unwrap();
-        *c
+        (*c).clone()
     }
     #[inline]
-    fn sub(&self, a: &str, b: &str) -> &str {
+    fn sub(&self, a: String, b: String) -> String {
         self.add(a, self.inv(b))
     }
     #[inline]
     // return the additive inverse of a character given the alphabet
-    fn inv(&self, c: &str) -> &str {
+    fn inv(&self, c: String) -> String {
         let pos = self.atoi_map.get(&c).unwrap();
         let neg_pos = self.length - pos;
         let inv_c = self.itoa_map.get(&neg_pos).unwrap();
-        *inv_c
+        (*inv_c).clone()
     }
 }
 
-impl TryFrom<String> for Alphabet<'_> {
+impl TryFrom<String> for Alphabet {
     type Error = Error;
     /// Will Error if s contains duplicate characters or if s is shorter than 2
     fn try_from(s: String) -> Result<Self, Error> {
         if s.len() < 2 {
             return Err(Error::ErrorShortABC);
         }
-        let value: Vec<&str> = s
+
+        let value: Vec<String> = s
             .to_uppercase()
-            .as_str()
             .graphemes(true)
+            .map(|s| s.to_string())
             .collect();
-        let length = s.len();
+
+        let length = value.len();
+
         let (atoi_map, itoa_map) = Self::build_maps(value.clone())?;
         Ok(Self {
             value,
@@ -166,12 +169,12 @@ impl TryFrom<String> for Alphabet<'_> {
 }
 
 /// Vigenere is where we will put our key and alphabet
-pub struct Vigenere<'a> {
+pub struct Vigenere {
     /// The alphabet is an ordered sequence of unique characters.
-    alphabet: Alphabet<'a>,
+    alphabet: Alphabet,
 
     // The key must be a string of characters all within the alphabet
-    key: Key<'a>,
+    key: Key,
 }
 // Mode to know whether we are encrypting or decryptiong
 pub enum Mode {
@@ -182,13 +185,13 @@ pub enum Mode {
     Decrypt,
 }
 
-impl Vigenere<'_> {
+impl Vigenere {
     fn valid_key_or_err(key: &str, alphabet: &Alphabet) -> Result<(), Error> {
         if key.is_empty() {
-            return Err(Error::ErrorShortKey)
+            return Err(Error::ErrorShortKey);
         }
         if !alphabet.is_key_valid(key.into()) {
-            return Err(Error::ErrorBadKeyChar)
+            return Err(Error::ErrorBadKeyChar);
         }
         Ok(())
     }
@@ -212,20 +215,21 @@ impl Vigenere<'_> {
     }
 
     /// Decrypts or encrypts text depending on mode
-    pub fn crypt(&self, text: &str, mode: Mode) -> Vec<&str> {
+    pub fn crypt(&self, text: &str, mode: Mode) -> Vec<String> {
         let key_mod = self.key.len();
         let mut key_pos: usize = 0;
-        let mut vec: Vec<&str> = Vec::with_capacity(text.len());
+        let mut vec: Vec<String> = Vec::with_capacity(text.len());
 
         let text = text.to_uppercase();
         for gr in text.graphemes(true) {
-            if !self.alphabet.contains(gr) {
+            let c = gr.to_string();
+            if !self.alphabet.contains(c.clone()) {
                 continue;
             }
             let key_char = self.key.char_from_position(key_pos);
-            vec.push( match mode {
-                Mode::Encrypt => { self.alphabet.add(gr, key_char) },
-                Mode::Decrypt => { self.alphabet.sub(gr, key_char)}
+            vec.push(match mode {
+                Mode::Encrypt => self.alphabet.add(c, key_char),
+                Mode::Decrypt => self.alphabet.sub(c, key_char),
             });
             key_pos += 1;
             key_pos %= key_mod;
@@ -233,10 +237,10 @@ impl Vigenere<'_> {
         vec
     }
 
-    pub fn encrypt(&self, text: &str) -> Vec<&str> {
+    pub fn encrypt(&self, text: &str) -> Vec<String> {
         self.crypt(text, Mode::Encrypt)
     }
-    pub fn decrypt(&self, text: &str) -> Vec<&str> {
+    pub fn decrypt(&self, text: &str) -> Vec<String> {
         self.crypt(text, Mode::Decrypt)
     }
 }
