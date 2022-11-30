e27677e8bf4e142127003fe568e369c465c65cc9
Jeffrey Goldberg <jeff@agilebits.com>
2022-11-30 10:47:35 -0600

    Way cleaner

--- Homework/2022-12-02/des_ex/src/main.rs
+++ Homework/2022-12-02/des_ex/src/main.rs
@@ -3,37 +3,32 @@ use des::{cipher::BlockEncrypt, Des};
 use rand::prelude::*;
 
 /// returns true iff comp(E(key, plaintext)) == E(comp(key), comp(plaintext))
+/// Does not parity check key bytes
 fn des_comp_check(key: [u8; 8], plaintext: [u8; 8]) -> bool {
-    // Should we check parity bits on the key? Nah.
-
-    let key_comp: [u8; 8] = key
-        .iter()
-        .map(|b| !b)
-        .collect::<Vec<u8>>()
-        .as_slice()
-        .try_into()
-        .expect("uh oh");
-
-    let pt_comp: [u8; 8] = plaintext
-        .iter()
-        .map(|b| !b)
-        .collect::<Vec<u8>>()
-        .as_slice()
-        .try_into()
-        .expect("shouldn't happen");
-
+    
+    // compute comp(E(K, pt))
     let cipher = Des::new(&key.into());
-
-    let cipher_comp = Des::new(&key_comp.into());
-
     let block = plaintext;
     cipher.encrypt_block(&mut block.into());
-    let comp_of_encrypted: Vec<u8> = block.iter().map(|b| !b).collect();
+    let comp_of_encrypted = comp_u8_8(&block);
 
-    cipher_comp.encrypt_block(&mut pt_comp.into());
-    let encryption_of_complements = pt_comp;
+    // compute E(comp(K), comp(pt))
+    let key_comp = comp_u8_8(&key);
+    let cipher_comp = Des::new(&key_comp.into());
+    let comp_block = comp_u8_8(&plaintext);
+    cipher_comp.encrypt_block(&mut comp_block.into());
+    let encryption_of_complements = comp_block;
 
-    comp_of_encrypted == encryption_of_complements.to_vec()
+    comp_of_encrypted == encryption_of_complements
+}
+
+fn comp_u8_8(arr: &[u8; 8]) -> [u8; 8] {
+    arr.iter()
+        .map(|b| !b)
+        .collect::<Vec<u8>>()
+        .as_slice()
+        .try_into()
+        .expect("uh oh. 8 should equal 8")
 }
 
 fn main() {
