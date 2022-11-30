e979ad6de51643cc77887e7228935ec3b7114e7e
Jeffrey Goldberg <jeff@agilebits.com>
2022-11-30 11:22:32 -0600

    mutability and into interact

--- Homework/2022-12-02/exercises/src/main.rs
+++ Homework/2022-12-02/exercises/src/main.rs
@@ -13,10 +13,7 @@ use anyhow::{anyhow, Result, ensure};
 
 // I still don't understand GenericArray, but this helped me get things working
 // https://stackoverflow.com/a/60336286/1304076
-
-type AesKey256 = GenericArray<u8, <Aes256 as KeySizeUser>::KeySize>;
 type AesBlock256 = GenericArray<u8, <Aes256 as BlockSizeUser>::BlockSize>;
-
 
 #[allow(unused)]
 fn main() {
@@ -25,22 +22,17 @@ fn main() {
         00000000 00000000 00000000 00000001");
     let ct = hex!("539B333B39706D149028CFE1D9D4A407");
 
-    let mut block: AesBlock256 = GenericArray::from(ct);
+    let cipher = Aes256::new(&key.into());
 
-    let key: &AesKey256 = GenericArray::from_slice(&key);
-    let cipher = Aes256::new(key);
-
+    // let mut block: AesBlock256 = GenericArray::from(ct);
+    let mut block: AesBlock256 =  ct.into();
     cipher.decrypt_block(&mut block);
     println!("Ex 3.8\n\t{}", hex::encode(block));
 
-
     // exercise 3.9 uses the same key, so I can keep cipher.
-
     let pt = hex!("296C93FDF499AAEB4194BABC2E63561D");
-    let mut block: AesBlock256 = GenericArray::from(pt);
-
-    cipher.encrypt_block(& mut block);
-    println!("Ex 3.9\n\t{}", hex::encode(block));
+    let mut block: AesBlock256 = pt.into();
     
-
+    cipher.encrypt_block(&mut block);
+    println!("Ex 3.9\n\t{}", hex::encode(block));
 }
