19a151d7e6360e14db8e942224c2ea533a9425a6
Jeffrey Goldberg <jeff@agilebits.com>
2022-11-30 11:40:56 -0600

    extracting into()

--- Homework/2022-12-02/des_ex/src/main.rs
+++ Homework/2022-12-02/des_ex/src/main.rs
@@ -1,6 +1,10 @@
+use crypto_common::BlockSizeUser;
+use crypto_common::generic_array::GenericArray;
 use des::cipher::KeyInit;
 use des::{cipher::BlockEncrypt, Des};
 use rand::prelude::*;
+
+type DesBlock = GenericArray<u8, <Des as BlockSizeUser>::BlockSize>;
 
 /// returns true iff comp(E(key, plaintext)) == E(comp(key), comp(plaintext))
 /// Does not parity check key bytes
@@ -8,16 +12,16 @@ fn des_comp_check(key: [u8; 8], plaintext: [u8; 8]) -> bool {
     
     // compute comp(E(K, pt))
     let cipher = Des::new(&key.into());
-    let block = plaintext;
-    cipher.encrypt_block(&mut block.into());
-    let comp_of_encrypted = comp_u8_8(&block);
+    let mut block: DesBlock = plaintext.into();
+    cipher.encrypt_block(&mut block);
+    let comp_of_encrypted = comp_u8_8(block.as_ref());
 
     // compute E(comp(K), comp(pt))
     let key_comp = comp_u8_8(&key);
     let cipher_comp = Des::new(&key_comp.into());
-    let comp_block = comp_u8_8(&plaintext);
-    cipher_comp.encrypt_block(&mut comp_block.into());
-    let encryption_of_complements = comp_block;
+    let mut comp_block: DesBlock = comp_u8_8(&plaintext).into();
+    cipher_comp.encrypt_block(&mut comp_block);
+    let encryption_of_complements = comp_block.as_ref();
 
     comp_of_encrypted == encryption_of_complements
 }
