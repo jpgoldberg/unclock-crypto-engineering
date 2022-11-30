44c611934e0a08bec1060b2086db1040256fb7c9
Jeffrey Goldberg <jeff@agilebits.com>
2022-11-30 00:25:59 -0600

    It's not pretty but it works

--- Homework/2022-12-02/des_ex/src/main.rs
+++ Homework/2022-12-02/des_ex/src/main.rs
@@ -1,41 +1,55 @@
-#[allow(unused)]
-use des::cipher::{generic_array::GenericArray, Block, BlockSizeUser, KeyInit, KeySizeUser};
-use des::{Des, cipher::BlockEncrypt};
+use des::cipher::{generic_array::GenericArray, BlockSizeUser, KeyInit, KeySizeUser};
+use des::{cipher::BlockEncrypt, Des};
+
+use rand::prelude::*;
 
 use anyhow::Result;
+
+#[allow(unused_imports)]
+use hex_literal::hex;
 
 type DesKey = GenericArray<u8, <Des as KeySizeUser>::KeySize>;
 type DesBlock = GenericArray<u8, <Des as BlockSizeUser>::BlockSize>;
 
+fn des_comp_test(key: [u8; 8], plaintext: [u8; 8]) -> Result<bool> {
+    // Should we check parity bits on the key? Nah.
 
-fn des_comp_test(key: [u8; 56], plaintext: [u8; 64]) -> Result<bool> {
+    let key_comp: Vec<u8> = key.iter().map(|b| !b).collect::<Vec<u8>>();
 
-    let key_comp: Vec<u8> = key
-        .iter()
-        .map(|b| !b)
-        .collect::<Vec<u8>>();
-
-    let pt_comp: Vec<u8> = plaintext
-        .iter()
-        .map(|b| !b)
-        .collect::<Vec<u8>>();
+    let pt_comp: Vec<u8> = plaintext.iter().map(|b| !b).collect::<Vec<u8>>();
 
     let key: &DesKey = GenericArray::from_slice(&key);
 
     let key_comp: &DesKey = GenericArray::from_slice(&key_comp);
-    
+
     let cipher = Des::new(key);
     let cipher_comp = Des::new(key_comp);
 
-    let pt_copy = plaintext.clone();
-
-    let mut block = GenericArray::from(plaintext);
-
+    let mut block: DesBlock = GenericArray::from(plaintext);
     cipher.encrypt_block(&mut block);
 
+    let pt_comp = pt_comp.as_slice();
+    let mut comp_block: DesBlock = GenericArray::clone_from_slice(pt_comp);
+    cipher_comp.encrypt_block(&mut comp_block);
 
-    Ok(true)
+    let comp1_encrypted_block: Vec<u8> = block.iter().map(|b| !b).collect();
+
+    Ok(comp1_encrypted_block == comp_block.to_vec())
 }
 fn main() {
+    let mut rng = rand::thread_rng();
+    let mut rand_key = [0u8; 8];
+    let mut rand_block = [0u8; 8];
 
+    let trials: u32 = 1000;
+    let mut fails: u32 = 0;
+    for _ in 1..=trials {
+        rng.fill_bytes(&mut rand_key);
+        rng.fill_bytes(&mut rand_block);
+
+        if  !des_comp_test(rand_key, rand_block).unwrap() {
+            fails += 1;
+        }
+    }
+    println!("{} failure(s) of {} trials", fails, trials);
 }
--- Homework/2022-12-02/exercises/src/main.rs
+++ Homework/2022-12-02/exercises/src/main.rs
@@ -1,9 +1,8 @@
 #[allow(unused_imports)]
 
-
-use crypto_common::{KeyInit, KeySizeUser, BlockSizeUser, Block,generic_array::GenericArray,};
-
-use aes::{Aes128, Aes256};
+use aes::Aes256;
+#[allow(unused_imports)]
+use aes::cipher::{KeyInit, KeySizeUser, BlockSizeUser, Block,generic_array::GenericArray,BlockEncrypt, BlockDecrypt};
 
 use hex_literal::hex;
 
@@ -11,18 +10,10 @@ use hex_literal::hex;
 #[allow(unused_imports)]
 use anyhow::{anyhow, Result, ensure};
 
-#[allow(dead_code)]
-#[derive(Debug,Clone)]
-enum Algorithm {
-    AES256,
-    AES128,
-    DES,
-}
 
 // I still don't understand GenericArray, but this helped me get things working
 // https://stackoverflow.com/a/60336286/1304076
 
-type AesKey128 = GenericArray<u8, <Aes128 as KeySizeUser>::KeySize>;
 type AesKey256 = GenericArray<u8, <Aes256 as KeySizeUser>::KeySize>;
 type AesBlock256 = GenericArray<u8, <Aes256 as BlockSizeUser>::BlockSize>;
 
@@ -34,21 +25,21 @@ fn main() {
         00000000 00000000 00000000 00000001");
     let ct = hex!("539B333B39706D149028CFE1D9D4A407");
 
-    let mut block = GenericArray::from(ct);
+    let mut block: AesBlock256 = GenericArray::from(ct);
 
-    let key = GenericArray::from_slice(&key);
+    let key: &AesKey256 = GenericArray::from_slice(&key);
     let cipher = Aes256::new(key);
 
-    aes::cipher::BlockDecrypt::decrypt_block(&cipher, &mut block);
+    cipher.decrypt_block(&mut block);
     println!("Ex 3.8\n\t{}", hex::encode(block));
 
 
     // exercise 3.9 uses the same key, so I can keep cipher.
 
     let pt = hex!("296C93FDF499AAEB4194BABC2E63561D");
-    let mut block = GenericArray::from(pt);
+    let mut block: AesBlock256 = GenericArray::from(pt);
 
-    aes::cipher::BlockEncrypt::encrypt_block(&cipher, & mut block);
+    cipher.encrypt_block(& mut block);
     println!("Ex 3.9\n\t{}", hex::encode(block));
     
 
