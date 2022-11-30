777cbdfb63c304f7477da8873759fb2ffaa6c323
Jeffrey Goldberg <jeff@agilebits.com>
2022-11-30 10:18:46 -0600

    Got into it

--- Homework/2022-12-02/des_ex/src/main.rs
+++ Homework/2022-12-02/des_ex/src/main.rs
@@ -1,35 +1,41 @@
-use des::cipher::{generic_array::GenericArray, BlockSizeUser, KeyInit, KeySizeUser};
+use des::cipher::KeyInit;
 use des::{cipher::BlockEncrypt, Des};
 use rand::prelude::*;
 
-type DesKey = GenericArray<u8, <Des as KeySizeUser>::KeySize>;
-type DesBlock = GenericArray<u8, <Des as BlockSizeUser>::BlockSize>;
-
-fn des_comp_test(key: [u8; 8], plaintext: [u8; 8]) -> bool {
+/// returns true iff comp(E(key, plaintext)) == E(comp(key), comp(plaintext))
+fn des_comp_check(key: [u8; 8], plaintext: [u8; 8]) -> bool {
     // Should we check parity bits on the key? Nah.
 
-    let key_comp: Vec<u8> = key.iter().map(|b| !b).collect::<Vec<u8>>();
+    let key_comp: [u8; 8] = key
+        .iter()
+        .map(|b| !b)
+        .collect::<Vec<u8>>()
+        .as_slice()
+        .try_into()
+        .expect("uh oh");
 
-    let pt_comp: Vec<u8> = plaintext.iter().map(|b| !b).collect::<Vec<u8>>();
+    let pt_comp: [u8; 8] = plaintext
+        .iter()
+        .map(|b| !b)
+        .collect::<Vec<u8>>()
+        .as_slice()
+        .try_into()
+        .expect("shouldn't happen");
 
-    let key: &DesKey = GenericArray::from_slice(&key);
+    let cipher = Des::new(&key.into());
 
-    let key_comp: &DesKey = GenericArray::from_slice(&key_comp);
+    let cipher_comp = Des::new(&key_comp.into());
 
-    let cipher = Des::new(key);
-    let cipher_comp = Des::new(key_comp);
+    let block = plaintext;
+    cipher.encrypt_block(&mut block.into());
+    let comp_of_encrypted: Vec<u8> = block.iter().map(|b| !b).collect();
 
-    let mut block: DesBlock = GenericArray::from(plaintext);
-    cipher.encrypt_block(&mut block);
+    cipher_comp.encrypt_block(&mut pt_comp.into());
+    let encryption_of_complements = pt_comp;
 
-    let pt_comp = pt_comp.as_slice();
-    let mut comp_block: DesBlock = GenericArray::clone_from_slice(pt_comp);
-    cipher_comp.encrypt_block(&mut comp_block);
-
-    let comp1_encrypted_block: Vec<u8> = block.iter().map(|b| !b).collect();
-
-    comp1_encrypted_block == comp_block.to_vec()
+    comp_of_encrypted == encryption_of_complements.to_vec()
 }
+
 fn main() {
     let mut rng = rand::thread_rng();
     let mut rand_key = [0u8; 8];
@@ -41,7 +47,7 @@ fn main() {
         rng.fill_bytes(&mut rand_key);
         rng.fill_bytes(&mut rand_block);
 
-        if !des_comp_test(rand_key, rand_block) {
+        if !des_comp_check(rand_key, rand_block) {
             fails += 1;
         }
     }
