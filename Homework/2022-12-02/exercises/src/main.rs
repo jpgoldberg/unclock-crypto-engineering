cac929ce73614a6749eb6d784a36196ad8dc1f14
Jeffrey Goldberg <jeff@agilebits.com>
2022-12-02 00:59:01 -0600

    pkcs padder

--- Homework/2022-12-02/exercises/src/main.rs
+++ Homework/2022-12-02/exercises/src/main.rs
@@ -103,9 +103,30 @@ fn ex_4_4() -> Result<()> {
     let mut cipher = Aes256CbcDec::new(&key.into(), &iv.into());
     cipher.decrypt_blocks_mut(&mut [block0, block1]);
 
-    println!("Ex 4.4: plaintext\n\t{:X?}\n\t{:X?}", block0, block1);
+    println!("Ex 4.4: plaintext\n\t{:02X?}\n\t{:02X?}", block0, block1);
 
     Ok(())
+}
+
+fn pkcs_padder(data: &[u8]) -> Vec<u8> {
+    // I really should get the blocksize from somewhere,
+    // but will assume 16 for now.
+    const BLOCKSIZE: usize = 16; // in bytes
+
+    let data_len = data.len();
+
+    // I could do this with bitwise operations. Maybe later.
+    let pbytes: usize = BLOCKSIZE - (data_len % BLOCKSIZE);
+    let mut v = vec![0u8; pbytes + data_len];
+    let padded = v.as_mut_slice();
+
+    // as long as BLOCKSIZE is not greater than u8:MAX + 1 this cast is ok.
+    let pad = vec![pbytes as u8; pbytes];
+
+    padded[..data_len].copy_from_slice(data);
+    padded[data_len..].copy_from_slice(&pad);
+
+    padded.to_vec()
 }
 
 fn main() {
@@ -117,6 +138,10 @@ fn main() {
             eprintln!("{:?}", e)
         }
     }
+
+    let unpadded5 = hex!("FF FF FF FF FF");
+    let _padded5 = pkcs_padder(&unpadded5);
+    println!("Padding\n\tIn: {:02X?}", unpadded5);
 }
 
 #[cfg(test)]
@@ -127,5 +152,25 @@ mod test {
     fn test_4_4() {
         let r = ex_4_4();
         assert!(r.is_ok());
+    }
+
+    #[test]
+    fn test_padding() {
+        let unpadded5 = hex!("FF FF FF FF FF");
+        let padded5 = hex!(
+            "FF FF FF FF FF 0B 0B 0B
+                                      0B 0B 0B 0B 0B 0B 0B 0B"
+        );
+
+        let result = pkcs_padder(&unpadded5);
+        assert_eq!(result, padded5);
+
+        let unpadded16 = [0u8; 16];
+        let mut padded16 = unpadded16.to_vec().clone();
+        padded16.append(&mut vec![16u8; 16]);
+
+        let r16 = pkcs_padder(&unpadded16);
+
+        assert_eq!(r16, padded16);
     }
 }
